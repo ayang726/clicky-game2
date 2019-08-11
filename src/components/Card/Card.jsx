46a7db59478ecb08e1514d090725b983f795a8db
AwesomePresent <yoursmile128@hotmail.com>
2019-08-10 19:01:37 -0700

    fixing loading problem

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,11 @@
 import React from "react"
 import "./Card.css"
+import images from "./images";
 
 export default function (props) {
-
-    const imgSrc = `./assets/images/${props.cardName}.png`
     return (
         <div className="card" onClick={props.cardClick} >
-            <img className="card-content" src={imgSrc} alt="..." name={props.cardName} />
+            <img className="card-content" src={images[props.cardName]} alt="..." name={props.cardName} />
         </div>
     )
 }
---
+++ src/components/Card/images/index.js
@@ -0,0 +1,17 @@
+const beth = require("./beth.png");
+const birdperson = require("./birdperson.png");
+const evilmorty = require("./evilmorty.png")
+const gianthead = require("./gianthead.png")
+const goldenford = require("./goldenford.png")
+const jerry = require("./jerry.png")
+const jessica = require("./jessica.png")
+const meeseeks = require("./meeseeks.png")
+const morty = require("./morty.png")
+const mr = require("./mr.png")
+const rick = require("./rick.png")
+const summer = require("./summer.png")
+
+export default {
+    beth, birdperson, evilmorty, gianthead,
+    goldenford, jerry, jessica, meeseeks, morty, mr, rick, summer
+}
