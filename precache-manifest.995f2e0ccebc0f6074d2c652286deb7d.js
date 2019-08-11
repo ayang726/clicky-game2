9e2fd88320510851d0f90dd548562b855818a47d
AwesomePresent <yoursmile128@hotmail.com>
2019-08-10 18:17:32 -0700

    Updates

---
+++ precache-manifest.995f2e0ccebc0f6074d2c652286deb7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ae71023d3edddfb1f599f6499ff3a8",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "50b0bfd50a54fa9661ff",
+    "url": "/clicky-game/static/css/main.43c2c563.chunk.css"
+  },
+  {
+    "revision": "ebdb112b752dc766ff98",
+    "url": "/clicky-game/static/js/2.0b2b00b5.chunk.js"
+  },
+  {
+    "revision": "50b0bfd50a54fa9661ff",
+    "url": "/clicky-game/static/js/main.bafd7821.chunk.js"
+  },
+  {
+    "revision": "d6181d266dfc64b77c10",
+    "url": "/clicky-game/static/js/runtime~main.160f185f.js"
+  }
+]);
